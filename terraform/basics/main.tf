19767051e55c044e03ef27cc901a632ff09b3708
raghu <raghus3190@gmail.com>
2020-03-19 01:13:02 +0000

    vars and output files

--- terraform/basics/main.tf
+++ terraform/basics/main.tf
@@ -1,4 +1,15 @@
-# Download the latest Ghost image
+# Download the latest Ghost Image
 resource "docker_image" "image_id" {
-  name = "ghost:latest"
+  name = "${var.image_name}"
 }
+
+# Start the Container
+resource "docker_container" "container_id" {
+  name  = "${var.container_name}"
+  image = "${docker_image.image_id.latest}"
+  ports {
+    internal = "${var.int_port}"
+    external = "${var.ext_port}"
+  }
+}
+
